99bcb52e31bf088887ab51e010c68bcec40284cc
Krantz <simenkrantzknudsen@gmail.com>
2019-01-23 16:28:11 +0100

    Added syntax to Elixir

--- Elixir/test.ex
+++ Elixir/test.ex
@@ -4,8 +4,43 @@ defmodule Math do
     end
 end
 
-# List
-list = [3.1415, :pie, "Apple"]
-["Test"] ++ list        # ["Test", 3.1415, :pie, "Apple"]
 
-list ++ ["Test"]        # [3.1415, :pie, "Apple", "Test"]
+# Anonymous functions -- two common practices
+sum = fn(a, b) -> a + b end
+sum.(2, 3) #Gives out 5
+
+sum = &(&1 + &2)
+sum.(2, 3) #Gives out 5
+
+
+# Functions and modules
+defmodule Greeter do
+    def hello(name) do
+        "Hello, " <> name
+    end
+end
+Greeter.hello("Simen")  # "Hello, Simen"
+
+defmodule Length do
+    def of([]), do: 0
+    def of([_ | tail]), do: 1 + of(tail)
+end
+Length.of []        # Gives 0
+Length.of [1, 3, 5] # Gives 3
+
+defmodule Greeter2 do
+    def hello(), do: "Hello, anon"
+    def hello(name), do: "Hello, " <> name
+    def hello(name1, name2), do: "Hello, #{name1} and #{name2}"
+end
+
+#Private funcs & Default value for argument
+defmodule PrivateGreeter do
+    def hello(name, language_code \\ "en") do
+        phrase(language_code) <> name
+    end
+
+    defp phrase("en"), do: "Hello, "
+    defp phrase("no"), do: "Hei, "
+    defp phrase("es"), do: "Hola, "
+end
