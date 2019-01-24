f12c3acb77cd8d1d91d0b311a72521176f2ba4b7
Krantz <simenkrantzknudsen@gmail.com>
2019-01-24 16:10:34 +0100

    Added Exercise 2, part 3 -- Erlang

--- Exercise2/Part3/Erlang/foo.erl
+++ Exercise2/Part3/Erlang/foo.erl
@@ -23,6 +23,7 @@ increment(_Ns, 0) ->
     ok;
 increment(Ns, TimesLeft) ->
     % TODO: Send a message to the number server to increment the number (HINT: messages are sent with the ! operator)
+    Ns ! increment_number
     increment(Ns, TimesLeft-1).
 
 
@@ -30,6 +31,7 @@ decrement(_Ns, 0) ->
     ok;
 decrement(Ns, TimesLeft) ->
     % TODO: Send a message to the number server to decrement the number (HINT: messages are sent with the ! operator)
+    Ns ! decrement_number
     decrement(Ns, TimesLeft-1).
 
 
