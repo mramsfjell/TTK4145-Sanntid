aacf31a90fa0a9e5abf68685bc070f3ca85a7736
Torbjfug <torbjfug@stud.ntnu.no>
2019-02-21 21:45:54 +0100

    Updated orderlist to store type of order

--- elevator/lib/lift/order_list.ex
+++ elevator/lib/lift/order_list.ex
@@ -2,69 +2,102 @@ defmodule Elevator.Orderlist do
   use GenServer
 
   #Orderlist layout
-  # [u0, u1, u2, d3, d2, d1,c0,c1,c2,c3]
   #%{up: [0 0 0 0], down: [0 0 0 0], cab: [0 0 0 0]}
 
-  def start_link(floors) do
-    GenServer.start_link(__MODULE__, %{floors: floors, orders: []}, [name: :order_list])
+  def start_link(floors) when is_integer(floors) do
+    GenServer.start_link(__MODULE__, %{floors: floors}, [name: :order_list])
   end
 
 
-  def add(server,floor,direction) do
-    GenServer.call(server, {:edit,floor,direction,1})
+  def add(pid,floor,button_type)when is_integer(floor) and is_atom(button_type) do
+    GenServer.call(pid, {:add,floor,button_type})
   end
 
-  def remove(server,floor,direction) do
-    GenServer.call(server, {:edit,floor,direction,0})
+  def remove(pid,floor,direction) when is_integer(floor) and is_atom(direction) do
+    GenServer.call(pid, {:remove,floor,direction})
   end
 
-  def get_orders(server) do
-    GenServer.call(server,{:get})
+  def get_orders(pid) do
+    GenServer.call(pid,{:get})
   end
 
-  def get_order(server,floor,direction) do
-    GenServer.call(server,{:get,floor,direction})
+  def order_at_floor?(pid,floor,direction) when is_integer(floor) and is_atom(direction) do
+    GenServer.call(pid,{:get,floor,direction})
   end
 
-  def stop(server) do
-    GenServer.stop(server)
+  def stop(pid) do
+    GenServer.stop(pid)
   end
 
-  #Helper function
-  defp get_order_list_index(state,floor,direction) do
-    case direction do
-      :down -> state.floors*2 - floor-1
-      :up -> floor
+  #Helper functions
+  defp get_order_list_index(floor,button_type) do
+    case button_type do
+      :hall_up -> floor
+      :cab -> floor
+      :hall_down -> floor - 1
     end
   end
 
   #Callbacks
 
   def init(state) do
-    empty_orderlist = List.duplicate(0,(state.floors*2-2))
-    new_state = state |> Map.replace!(:orders, empty_orderlist)
+    orders = %{
+      cab: List.duplicate(0, state.floors),
+      hall_up: List.duplicate(0, state.floors),
+      hall_down: List.duplicate(0, state.floors)
+    }
+    new_state = state |> Map.put(:orders,orders)
     {:ok,new_state}
   end
 
-  def handle_call({:edit,floor,direction,value},_from, state) do
+  def handle_call({:add,floor,button_type},_from, state) do
     if 0 <= floor and floor < state.floors do
-      order_list_index = get_order_list_index(state,floor,direction)
-      new_orders = List.insert_at(state.orders, order_list_index , value)
-      new_state = Map.replace!(state, :orders, new_orders)
+      new_list = get_in(state,[:orders,button_type])
+                 |> List.update_at(floor, &(&1 = 1))
+
+      new_state = put_in(state,[:orders,button_type],new_list)
+      new_state |> inspect |> IO.puts
       {:reply,:ok,new_state}
     else
       {:reply,{:error,:nonexistent_floor},state}
     end
   end
 
+
   def handle_call({:get},_from, state) do
     {:reply,{:ok,state.orders},state}
   end
 
   def handle_call({:get,floor,direction},_from,state) do
-    index = get_order_list_index(state,floor,direction)
-    order = state.orders |> Enum.fetch(index)
-    {:reply,order,state}
+    order? =
+    case direction do
+      :up ->
+        state.orders.hall_up |> Enum.fetch!(floor) == 1 or
+        state.orders.cab |> Enum.fetch!(floor) == 1
+      :down ->
+        state.orders.hall_down |> Enum.fetch!(floor) == 1 or
+        state.orders.cab |> Enum.fetch!(floor) == 1
+    end
+    {:reply,order?,state}
   end
 
+  def handle_call({:remove,floor,direction},_from,state) do
+    if 0 <= floor and floor < state.floors do
+      new_state =
+        case direction do
+          :up ->
+            new_list = state.orders.hall_up |> List.update_at(floor, &(&1 = 0))
+            put_in(state,[:orders,:hall_up],new_list)
+          :down ->
+            new_list = state.orders.hall_down |> List.update_at(floor, &(&1 = 0))
+            put_in(state,[:orders,:hall_down],new_list)
+        end
+      cab_list = state.orders.cab |> List.update_at(floor, &(&1 = 0))
+      new_state = put_in(new_state,[:orders,:cab],cab_list)
+      IO.puts inspect new_state.orders
+      {:reply,:ok,new_state}
+    else
+      {:reply,{:error,:nonexistent_floor},state}
+    end
+  end
 end
