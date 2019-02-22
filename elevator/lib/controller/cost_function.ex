64936a90c7f54122bf48f9bcace8cbf919a226f9
Krantz <simenkrantzknudsen@gmail.com>
2019-02-22 13:16:09 +0100

    Cost function update

--- elevator/lib/controller/cost_function.ex
+++ elevator/lib/controller/cost_function.ex
@@ -1,31 +1,34 @@
 defmodule CostFunction do
-    # Check if an elevator is in IDLE
-    # Traverse through order list
-    # Calculate cost, given state, current direction, last passed floor
+    # INPUT  -  current order_list to each lift
+    #           last passed floor
+    #           new order (floor, type - :cab/:hall)
+    #           direction
+    #           current state
 
-
+    ## Unsure if this is applicable
     def direction_to_int(direction) do
         case direction do
             :up -> 1
             :down -> -1
             :stop -> 0
-            _ -> {:error,:nonexistent_dir}
+            _ -> {:error,:invalid_direction}
         end
     end
 
-    def traverse_order_list() do
-        
+
+
+    def traverse_order_list(direction,order_list,order) do
+        # Go through each element in order list
     end
 
-    def calculate_cost(state,curr_floor,state) do
-        case state do
-            :idle ->
-
-            :mooving ->
-
-            :init ->
-        end
+    # When cab call 
+    
+    def calculate_cost(:idle,curr_floor,{_type,floor},_direction) do
+        abs(curr_floor - floor)
     end
 
+    def calculate_cost(state,curr_floor,{type,floor},direction) do
+        # When mooving or door_open
+    end
 
 end
