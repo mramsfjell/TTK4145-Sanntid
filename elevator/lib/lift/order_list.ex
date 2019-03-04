048e72f91c734422893563a3f96b43b1a04bec1e
Torbjfug <torbjfug@stud.ntnu.no>
2019-03-04 10:24:00 +0100

    changed orderlist to nested maps

--- elevator/lib/lift/order_list.ex
+++ elevator/lib/lift/order_list.ex
@@ -6,8 +6,6 @@ defmodule Elevator.Orderlist do
   #Orderlist layout
   #%{active: %{node%{time%{order}}}, complete: %{node%{time%{order}}}, :floors}
 
-  #Orderlist layout
-  #%{active: [orders], complete: [orders], :floors}
 
   def start_link([floors]) when is_integer(floors) do
     GenServer.start_link(__MODULE__, %{floors: floors}, [name: @name])
@@ -19,17 +17,21 @@ defmodule Elevator.Orderlist do
   end
 
   def order_done(order) do
-    GenServer.call(@name, {:order_done,order})
+    GenServer.multi_call([Node.self|Node.list],@name, {:order_done,order})
   end
 
   def get_orders() do
     GenServer.call(@name,{:get})
   end
 
-  def order_at_floor?(floor,direction)
+  def get_orders_at_floor(floor,direction)
   when is_integer(floor) and is_atom(direction)
   do
     GenServer.call(@name,{:get,floor,direction})
+  end
+
+  def order_complete?(order) do
+    GenServer.call(@name, {:get,order})
   end
 
   def stop() do
@@ -57,40 +59,39 @@ defmodule Elevator.Orderlist do
   #Callbacks
   def init(config) do
     new_state = %{
-      active_self: [],
-      complete: [],
-      active_system: [],
+      active: %{Node.self => %{}},
+      complete: %{Node.self => %{}},
       floors: config.floors,
     }
+    :net_kernel.monitor_nodes(true)
     {:ok,new_state}
   end
 
   def handle_call({:add,order},_from, state) do
     new_state =
-    if order.node == Node.self do
-      state |> Map.update!(:active_self,&([order|&1]))
+    if  Map.has_key?(state.active, order.node) do
+      put_in(state, [:active,order.node,order.time],order)
     else
-      state |> Map.update!(:active_system,&([order|&1]))
+      put_in(state, [:active,order.node], %{order.time => order})
     end
     {:reply,:ok,new_state}
   end
 
   def handle_call({:order_done,order},_from,state) do
-    order_list =
-    if order.node ==  Node.self do
-      :active_self
-    else
-      :active_system
-    end
-    completed_orders = state |> Map.get(order_list) |> Enum.filter(&(&1 == order))
-    active_orders = Map.get(state,order_list) -- completed_orders
     new_state =
-    if length(completed_orders) > 0 do
-      state
-        |> Map.put(order_list,active_orders)
-        |> Map.update!(:complete,&([completed_orders|&1]))
+    case pop_in(state, [:active,order.node,order.time]) do
+      {nil,state} ->
+        IO.puts "non existent order"
+        state
+      {_complete_order,new_state} ->
+        IO.puts "order removed"
+        if  Map.has_key?(new_state.complete, order.node) do
+          put_in(new_state, [:complete,order.node,order.time],order)
+        else
+          put_in(new_state, [:complete,order.node], %{order.time => order})
+        end
     end
-    IO.puts(inspect(state))
+    IO.puts(inspect(new_state))
     {:reply,:ok,new_state}
   end
 
@@ -100,17 +101,41 @@ defmodule Elevator.Orderlist do
   end
 
   def handle_call({:get,floor,direction},_from,state) do
-    order? = state.active_self
-    |> Enum.any?(&(order_at_floor(&1,floor,direction)))
-    {:reply,order?,state}
+    orders = get_in(state,[:active,Node.self])
+      |> Map.values
+      |> Enum.filter(&(order_at_floor(&1,floor,direction)))
+    {:reply,orders,state}
   end
+
+  def handle_call({:get,order},_from,state) do
+    complete? = order in (get_in(state, [:complete,order.node]) |> Map.values)
+    {:reply,complete?,state}
+  end
+
+  def handle_info({:nodedown, node},state) do
+    case get_pop_in(state,[:active,node]) do
+      {node_orders,state} ->
+        responsible = Enum.filter(node_orders, &(&1.watch_node == Node.self))
+        Enum.each(responsible, fn order->
+          case order.button_type do
+            :cab ->
+            oter ->
+              IO.puts("add order to Auction")
+              Process.sleep(500)
+              IO.puts("order added")
+            end
+          end
+      )
+  end
+end
+
 end
 
 defmodule Order do
   @valid_order [:hall_down, :cab, :hall_up]
   @valid_floor 0..3
   @enforce_keys [:floor,:button_type]
-  defstruct [:floor,:button_type,:time_stamp,node: nil,watch_node: nil]
+  defstruct [:floor,:button_type,:time,node: nil,watch_node: nil]
 
   def new(floor,button_type)
     when floor in @valid_floor and  button_type in @valid_order
@@ -118,7 +143,7 @@ defmodule Order do
       %Order{
         floor: floor,
         button_type: button_type,
-        time_stamp: Time.utc_now()|> Time.truncate(:second),
+        time: Time.utc_now()|> Time.truncate(:second),
         node: Node.self
       }
   end
