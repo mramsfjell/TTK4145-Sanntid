d6aa3e7ddc01452e6b6b6c5c80eb581db425ffc2
torbjfug <torbjfug@stud.ntnu.no>
2019-03-24 16:04:58 +0100

    Made the lift restart if it gets an infeasable order

--- lift_project/lib/lift_project/lift.ex
+++ lift_project/lib/lift_project/lift.ex
@@ -42,6 +42,8 @@ defmodule Lift do
   # Callbacks --------------------------------------------
   def init([]) do
     Driver.set_door_open_light(:off)
+    Driver.set_motor_direction(:stop)
+    Process.sleep(500)
 
     data =
       case Driver.get_floor_sensor_state() do
@@ -159,7 +161,19 @@ defmodule Lift do
     end
   end
 
-  defp new_order_event(%Lift{} = data, %Order{} = order) do
+  defp new_order_event(
+         %Lift{floor: current_floor, dir: :up} = data,
+         %Order{floor: target_floor} = order
+       )
+       when current_floor <= target_floor do
+    add_order(data, order)
+  end
+
+  defp new_order_event(
+         %Lift{floor: current_floor, dir: :down} = data,
+         %Order{floor: target_floor} = order
+       )
+       when current_floor >= target_floor do
     add_order(data, order)
   end
 
